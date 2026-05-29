73580d99365b0eb3866281cea334e258b9feac94
azizbenkhaled1919-ux <azizbenkhaled1919@gmail.com>
2026-05-29 17:50:20 +0200

    main.tf

--- main.tf
+++ main.tf
@@ -187,11 +187,12 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "web" {
-  count                  = var.instance_count
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.private[count.index % length(aws_subnet.private)].id
-  vpc_security_group_ids = [aws_security_group.ec2.id]
+  count                       = var.instance_count
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public[count.index % length(aws_subnet.public)].id
+  vpc_security_group_ids      = [aws_security_group.ec2.id]
+  associate_public_ip_address = true        # ← ajout important
 
   user_data = <<-EOF
               #!/bin/bash
